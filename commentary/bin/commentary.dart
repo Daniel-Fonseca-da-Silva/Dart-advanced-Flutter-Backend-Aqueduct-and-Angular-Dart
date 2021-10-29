50726cf8576255ad87059c4873c8bb47be2c4b7d
Daniel-Fonseca-da-Silva <developer-web@programmer.net>
2021-10-29 01:15:41 -0300

    Created Commentary

---
+++ commentary/bin/commentary.dart
@@ -0,0 +1,14 @@
+/*
+ This import, will import one file and load her like one variable
+*/
+
+import 'package:commentary/commentary.dart' as commentary;
+
+// This is the point of start the application
+void main(List<String> arguments) {
+
+  // Function print show a message in your screen
+  print('Hello world: ${commentary.calculate()}!');
+
+  print('This is anything');
+}
---
+++ commentary/test/commentary_test.dart
@@ -0,0 +1,8 @@
+import 'package:commentary/commentary.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
