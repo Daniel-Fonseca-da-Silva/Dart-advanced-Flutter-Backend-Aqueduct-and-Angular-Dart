7b3a031877396b9e59c9fdd6b3f41abd92ee07cd
Daniel-Fonseca-da-Silva <developer-web@programmer.net>
2021-10-29 15:25:40 -0300

    Bool variable

---
+++ bool-variable/bin/bool_variable.dart
@@ -0,0 +1,14 @@
+main() {
+
+  bool today = true;
+
+  print('Hey Jhon I saw in tv that today will have sun, do you agree?');
+  print("Me too and this is very $today");
+
+  // Brake line this is my power because C++ is my father
+  print("\n");
+
+  print("What's your variable you used in this program");
+  print('I used a type of ${today.runtimeType} variable in my program');
+
+}
