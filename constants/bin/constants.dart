daaa992a9930231aaa59529059b21b42f02ac989
Daniel-Fonseca-da-Silva <developer-web@programmer.net>
2021-10-30 22:28:17 -0300

    Constants

---
+++ constants/bin/constants.dart
@@ -0,0 +1,16 @@
+main () {
+
+  // Create a constant in runtime
+  const pi = 3.14;
+  print('The value constant of PI is $pi');
+
+  // Create a constant in compilation time
+
+  final daniel = new People();
+  print(daniel);
+
+}
+
+class People {
+
+}
